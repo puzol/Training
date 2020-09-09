8fc1b196940f7a3dcbe19930999e294ca542a2ae
puzol <51291320+puzol@users.noreply.github.com>
2020-09-09 21:06:27 +0200

    animated hamburger toggle

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const logoAndTitle = document.getElementById("logoAndTitle");
 const logo = document.getElementById("logo");
 const heroTitle = document.getElementById("heroTitle");
 const navList = document.getElementById("navList");
+const hamburger = document.getElementById("hamburger");
+const hamburgerLine1 = document.getElementById("hamburgerLine1");
+const hamburgerLine2 = document.getElementById("hamburgerLine2");
+const hamburgerLine3 = document.getElementById("hamburgerLine3");
+let counter = 1;
 
 
 const moveInLeft =(id)=>{
@@ -22,6 +27,27 @@ const brdrAndBck =(id)=>{
     id.style.border = "1px solid #C7AE58";
 }
 
+const hamburgerAnimation=()=>{
+
+    if(counter == 1){
+        hamburgerLine1.style.top="50%";
+        hamburgerLine1.style.transform="translate(0, -50%) rotate(45deg)";
+        hamburgerLine2.style.opacity="0";
+        hamburgerLine3.style.top="50%";
+        hamburgerLine3.style.transform="translate(0, -50%) rotate(-45deg)";
+        counter = 2;
+        console.log(counter);
+    }else if(counter == 2){
+        hamburgerLine1.style.top="16px";
+        hamburgerLine1.style.transform="translate(0, 0) rotate(0)";
+        hamburgerLine2.style.opacity="1";
+        hamburgerLine3.style.top="40px";
+        hamburgerLine3.style.transform="translate(0, 0) rotate(0)";
+        counter = 1;
+        console.log(counter);
+    }
+}
+
 window.onload = function(){
     setTimeout(function(){
         brdrAndBck(logoAndTitle)
@@ -32,5 +58,8 @@ window.onload = function(){
     setTimeout(function(){
         moveInRigth(navList)
     },1700);
+}
 
+hamburger.onclick = function(){
+    hamburgerAnimation();
 }
