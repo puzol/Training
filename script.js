051b71aa181c649282f3b31a158bdf34f9e5db53
puzol <51291320+puzol@users.noreply.github.com>
2020-09-09 19:48:25 +0200

    anim

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const logoAndTitle = document.getElementById("logoAndTitle");
+const logo = document.getElementById("logo");
+const heroTitle = document.getElementById("heroTitle");
 
-const moveIn =()=>{
-    logoAndTitle.style.left="50%";
-    logoAndTitle.style.background = "rgba(0,0,0,0.3)";
-    logoAndTitle.style.border = "1px solid #C7AE58";
+
+const moveIn =(id)=>{
+    id.style.left="50%";
+}
+
+const appear=(id1,id2)=>{
+    id1.style.opacity="1";
+    id2.style.opacity="1";
+}
+
+const brdrAndBck =(id)=>{
+    id.style.background = "rgba(0,0,0,0.3)";
+    id.style.border = "1px solid #C7AE58";
 }
 
 window.onload = function(){
     setTimeout(function(){
-        moveIn()
-    },1000)
+        brdrAndBck(logoAndTitle)
+    },800);
+    setTimeout(function(){
+        appear(logo, heroTitle)
+    },1700);
 }
