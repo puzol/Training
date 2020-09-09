2881c70c080714761dd0fed4cef9ce68646fae00
puzol <51291320+puzol@users.noreply.github.com>
2020-09-09 22:38:05 +0200

    animirani meni za mob

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const hamburger = document.getElementById("hamburger");
 const hamburgerLine1 = document.getElementById("hamburgerLine1");
 const hamburgerLine2 = document.getElementById("hamburgerLine2");
 const hamburgerLine3 = document.getElementById("hamburgerLine3");
+const navId = document.getElementById("navId");
 let counter = 1;
 
 
@@ -27,6 +28,33 @@ const brdrAndBck =(id)=>{
     id.style.border = "1px solid #C7AE58";
 }
 
+const respMenuOpen =()=>{
+    navId.style.display="flex";
+    navList.classList.remove("nav-list");
+    navList.style.opacity="0"
+    setTimeout(function(){
+        navId.style.background="#C7AE58";
+        navId.style.height="80vh";
+        navList.classList.add("nav-list-resp");
+    }, 300);
+    setTimeout(function(){
+        navList.style.opacity="1";
+    },1000);
+}
+
+const respMenuClose =()=>{
+    navList.style.opacity="0";
+    setTimeout(function(){
+        navId.style.height="0";
+    }, 300);
+    setTimeout(function(){
+        navId.style.background="transparent";
+        navList.classList.add("nav-list");
+        navList.classList.remove("nav-list-resp");
+        navId.style.display="none";
+    },1000)
+}
+
 const hamburgerAnimation=()=>{
 
     if(counter == 1){
@@ -35,6 +63,7 @@ const hamburgerAnimation=()=>{
         hamburgerLine2.style.opacity="0";
         hamburgerLine3.style.top="50%";
         hamburgerLine3.style.transform="translate(0, -50%) rotate(-45deg)";
+        respMenuOpen();
         counter = 2;
         console.log(counter);
     }else if(counter == 2){
@@ -43,6 +72,7 @@ const hamburgerAnimation=()=>{
         hamburgerLine2.style.opacity="1";
         hamburgerLine3.style.top="40px";
         hamburgerLine3.style.transform="translate(0, 0) rotate(0)";
+        respMenuClose();
         counter = 1;
         console.log(counter);
     }
